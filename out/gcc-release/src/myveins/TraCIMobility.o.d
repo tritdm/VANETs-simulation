cb374863fa94fd8ec96384c98e180e16beab0223
tritdm <20520326@gm.uit.edu.vn>
2023-05-30 16:09:19 +0700

    only keep used files

--- out/gcc-release/src/myveins/TraCIMobility.o.d
+++ out/gcc-release/src/myveins/TraCIMobility.o.d
@@ -1,39 +1,22 @@
-../out/gcc-release/src/myveins/TraCIDemo11p.o: myveins/TraCIDemo11p.cc \
- myveins/TraCIDemo11p.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/application/ieee80211p/DemoBaseApplLayer.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseApplLayer.h \
+../out/gcc-release/src/myveins/TraCIMobility.o: myveins/TraCIMobility.cc \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseMobility.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/veins.h \
  /home/mike/Documents/dsrc_cv2x/omnetpp-6.0/include/omnetpp.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseLayer.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BatteryAccess.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseModule.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/HostState.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseBattery.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/PassedMessage.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/SimpleAddress.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/Consts80211p.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/ConstsPhy.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/messages/BaseFrame1609_4_m.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/SimpleAddress_m.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/common_m.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/messages/DemoServiceAdvertisement_m.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Coord_m.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Coord.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FWMath.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/messages/DemoSafetyMessage_m.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/ChannelAccess.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FindModule.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseMobility.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Move.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Heading.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mac/ieee80211p/DemoBaseApplLayerToMac1609_4Interface.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/NetwToMacControlInfo.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FindModule.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Heading.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h \
@@ -51,20 +34,15 @@
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/EnumBitset.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
  /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
- /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/application/traci/TraCIDemo11pMessage_m.h
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h
 
-myveins/TraCIDemo11p.h:
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h:
 
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/application/ieee80211p/DemoBaseApplLayer.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseApplLayer.h:
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseMobility.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/veins.h:
 
 /home/mike/Documents/dsrc_cv2x/omnetpp-6.0/include/omnetpp.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseLayer.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BatteryAccess.h:
 
@@ -74,55 +52,25 @@ myveins/TraCIDemo11p.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseBattery.h:
 
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/PassedMessage.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/SimpleAddress.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/Consts80211p.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/ConstsPhy.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/messages/BaseFrame1609_4_m.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/SimpleAddress_m.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/common_m.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/messages/DemoServiceAdvertisement_m.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Coord_m.h:
-
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Coord.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FWMath.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/messages/DemoSafetyMessage_m.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/ChannelAccess.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FindModule.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseMobility.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Move.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h:
 
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Heading.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mac/ieee80211p/DemoBaseApplLayerToMac1609_4Interface.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/NetwToMacControlInfo.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h:
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FindModule.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h:
 
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h:
+
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Heading.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h:
 
@@ -159,5 +107,3 @@ myveins/TraCIDemo11p.h:
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h:
 
 /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
-
-/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/application/traci/TraCIDemo11pMessage_m.h:
---
+++ out/gcc-release/src/myveins/TraCIVehicleInserter.o.d
@@ -0,0 +1,113 @@
+../out/gcc-release/src/myveins/TraCIVehicleInserter.o: \
+ myveins/TraCIVehicleInserter.cc \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIVehicleInserter.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/veins.h \
+ /home/mike/Documents/dsrc_cv2x/omnetpp-6.0/include/omnetpp.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Coord.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FWMath.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Heading.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FindModule.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Move.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/EnumBitset.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseMobility.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BatteryAccess.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseModule.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/HostState.h \
+ /home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseBattery.h
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIVehicleInserter.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/veins.h:
+
+/home/mike/Documents/dsrc_cv2x/omnetpp-6.0/include/omnetpp.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Coord.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FWMath.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Heading.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/FindModule.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/Move.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/EnumBitset.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseMobility.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BatteryAccess.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseModule.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/utils/HostState.h:
+
+/home/mike/Documents/dsrc_cv2x/veins-veins-5.2/src/veins/base/modules/BaseBattery.h:
